4c1265ac9fec7281b953a9457d554025af483f2a
Sandeep Sokle <60534028+SandeepSokle@users.noreply.github.com>
2021-09-21 21:27:07 +0530

    start

---
+++ src/Leftnav.jsx
@@ -0,0 +1,37 @@
+
+import {useState} from "react"
+import "./CSS/Leftnav.css"
+
+let LeftNav = ()=>{
+
+const [isActive, setIsActive] = useState("node");
+
+        console.log(isActive);
+    return <ul className="list-group col-3">
+  <li className={`list-group-item`} onClick = {()=>{
+      setIsActive("all")
+  }}><b>Zendenta</b></li>
+  <li className={`list-group-item ${isActive === "over" ? "Active" : ""}`}onClick = {()=>{
+      setIsActive("over")
+  }}>Overview</li>
+  <li className={`list-group-item ${isActive === "cal" ? "Active" : ""}`}onClick = {()=>{
+      setIsActive("cal")
+  }}>Calender</li>
+  <li className={`list-group-item ${isActive === "pat" ? "Active" : ""}`}onClick = {()=>{
+      setIsActive("pat")
+  }}>Patient List</li>
+  <li className={`list-group-item ${isActive === "mes" ? "Active" : ""}`}onClick = {()=>{
+      setIsActive("mes")
+  }}>Messages</li>
+  <li className={`list-group-item ${isActive === "pay" ? "Active" : ""}`}onClick = {()=>{
+      setIsActive("pay")
+  }}>Payment Information</li>
+  <li className={`list-group-item ${isActive === "set" ? "Active" : ""}`}onClick = {()=>{
+      setIsActive("set")
+  }}>Settings</li>
+</ul>
+}
+
+
+
+export default LeftNav;
